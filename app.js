6e0f606b6488f87a16f91bb0480a87c00f43a85a
bucketbyte <formordestroyer@gmail.com>
2025-07-06 10:05:05 +0545

    Added search filter, filter options and

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const addNewMovie = document.getElementById("addNewMovie");
+const searchMovie = document.getElementById("search-movie");
+const selectFilter = document.getElementById("select-filter");
+const clearListBtn = document.getElementById("clear-list");
+
+selectFilter.value = "all";
+
 const movieList = document.querySelector(".movie-list");
 
 const popup = document.querySelector(".popup-add-movie");
@@ -15,14 +21,24 @@ const favorite = document.getElementById("favorite");
 const addMovieForm = document.getElementById("add-movie");
 
 let watchedMovies = [];
+loadFromLocalStorage();
 
-function renderMovie() {
+function renderMovie(movieArray) {
   movieList.innerHTML = "";
 
-  watchedMovies.forEach(function (movie) {
+  movieArray.forEach(function (movie) {
     //create cards
     const card = document.createElement("div");
     card.classList.add("movie-card");
+
+    let uniqueMovieId = `${movie.movie_title}_${
+      movie.movie_year
+    }_${Date.now()}`;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.id = `${uniqueMovieId}`;
+    deleteBtn.textContent = "X";
 
     const title = document.createElement("h2");
     title.textContent = movie.movie_title;
@@ -53,17 +69,51 @@ function renderMovie() {
     const status = document.createElement("p");
     status.textContent = `${watchedStat.textContent}  |  ${favoriteStat.textContent}`;
 
+    card.append(deleteBtn);
     card.append(posterImg);
     card.append(title);
     card.append(director);
     card.append(year);
     card.append(status);
-    // card.append(watchedStat);
-    // card.append(favoriteStat);
     movieList.append(card);
   });
+  console.log(watchedMovies);
 }
 
+function searchFilterFunc(searchQuery) {
+  let searchedMovie = watchedMovies.filter((movie) => {
+    return movie.movie_title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+  renderMovie(searchedMovie);
+}
+
+function selectFilterFunc(selectedFilter) {
+  if (selectedFilter === "all") {
+    renderMovie(watchedMovies);
+    return;
+  }
+  let filteredMovies = watchedMovies.filter((movie) => {
+    if (selectedFilter === "watched") {
+      return movie.movie_watched === true;
+    } else if (selectedFilter === "unwatched") {
+      return movie.movie_watched === false;
+    } else if (selectedFilter === "favorite") {
+      return movie.movie_favorite === true;
+    }
+  });
+
+  renderMovie(filteredMovies);
+}
+
+function saveTOLocalStorage() {
+  localStorage.setItem("movies-list", JSON.stringify(watchedMovies));
+}
+function loadFromLocalStorage() {
+  watchedMovies = localStorage.getItem("movies-list")
+    ? JSON.parse(localStorage.getItem("movies-list"))
+    : [];
+  renderMovie(watchedMovies);
+}
 addNewMovie.addEventListener("click", function () {
   popup.classList.add("active");
 });
@@ -83,8 +133,25 @@ addMovieForm.addEventListener("submit", function (e) {
     movie_favorite: favorite.checked,
   };
   watchedMovies.push(movie);
+  saveTOLocalStorage();
   popup.classList.remove("active");
   addMovieForm.reset();
-  console.log(watchedMovies);
-  renderMovie();
+  renderMovie(watchedMovies);
+});
+
+searchMovie.addEventListener("input", function () {
+  let searchQuery = searchMovie.value.trim();
+
+  searchFilterFunc(searchQuery);
+});
+
+selectFilter.addEventListener("change", function () {
+  let selectedFilter = selectFilter.value;
+
+  selectFilterFunc(selectedFilter);
+});
+
+clearListBtn.addEventListener("click", function () {
+  movieList.innerHTML = "";
+  watchedMovies.length = 0;
 });
